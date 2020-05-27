803daa6405019e67a93fed2858001e5277bcf1dd
unknown <alstonro45@gmail.com>
2020-05-27 18:44:09 +0530

    27-05-2020

---
+++ coding_solutions/DAY9(27-05-2020)/BubbleSort.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void bubble_sort(int[], int);
+void main() {
+   int arr[30], num, i;
+   printf("\nEnter no of elements :");
+   scanf("%d", &num);
+   printf("\nEnter array elements :");
+   for (i = 0; i < num; i++)
+      scanf("%d", &arr[i]);
+   bubble_sort(arr, num);
+}
+ 
+void bubble_sort(int iarr[], int num) {
+   int i, j, k, temp;
+ 
+   printf("\nUnsorted Data:");
+   for (k = 0; k < num; k++) {
+      printf("%5d", iarr[k]);
+   }
+ 
+   for (i = 1; i < num; i++) {
+      for (j = 0; j < num - 1; j++) {
+         if (iarr[j] > iarr[j + 1]) {
+            temp = iarr[j];
+            iarr[j] = iarr[j + 1];
+            iarr[j + 1] = temp;
+         }
+      }
+ 
+      printf("\nAfter pass %d : ", i);
+      for (k = 0; k < num; k++) {
+         printf("%5d", iarr[k]);
+      }
+   }
+   printf("\ntotal passes are :%d",i-1);
+}
