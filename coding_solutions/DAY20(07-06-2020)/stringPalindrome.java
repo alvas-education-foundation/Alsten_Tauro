3357ca06da4d8fba95056062f1dc11beb67ff8d0
unknown <alstonro45@gmail.com>
2020-06-07 19:31:33 +0530

    07-06-2020

---
+++ coding_solutions/DAY20(07-06-2020)/stringPalindrome.java
@@ -0,0 +1,47 @@
+import java.util.*;
+public class Main  { 
+    static boolean checkPalin(String word) 
+    { 
+        int n = word.length(); 
+        word = word.toLowerCase(); 
+        for (int i = 0; i < n; i++, n--) 
+            if (word.charAt(i) !=  
+                       word.charAt(n - 1)) 
+                return false; 
+        return true; 
+    } 
+    static String longestPalin(String str) 
+    { 
+        str = str + " "; 
+        String longestword = "", word = ""; 
+        int length, length1 = 0; 
+        for (int i = 0; i < str.length(); i++)  
+        { 
+            char ch = str.charAt(i); 
+            if (ch != ' ') 
+                word = word + ch; 
+            else { 
+                length = word.length(); 
+                if (checkPalin(word) &&  
+                             length > length1) 
+                { 
+                    length1 = length; 
+                    longestword = word; 
+                } 
+  
+                word = ""; 
+            } 
+        } 
+        return longestword; 
+    } 
+    public static void main(String args[]) 
+    {   
+        String s = new String("My name is bob renner"); 
+  
+        if (longestPalin(s) == "") 
+            System.out.println("No Palindrome"
+                            + " Word"); 
+        else
+            System.out.println(longestPalin(s)); 
+    } 
+}
