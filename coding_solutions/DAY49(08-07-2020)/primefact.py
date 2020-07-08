02d578d4b334a07024ef6041b5d3a9a7411495a5
unknown <alstonro45@gmail.com>
2020-07-08 20:01:07 +0530

    08-07-2020

---
+++ coding_solutions/DAY49(08-07-2020)/primefact.py
@@ -0,0 +1,17 @@
+import math
+def maxPrimeFactors (n):
+    maxPrime = -1
+    while n % 2 == 0:
+        maxPrime = 2
+        n >>= 1    
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            maxPrime = i
+            n = n / i
+    if n > 2:
+        maxPrime = n
+    return int(maxPrime)
+n = 15
+print(maxPrimeFactors(n))
+n = 25698751364526
+print(maxPrimeFactors(n))
